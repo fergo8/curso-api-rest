74fe96c91743bbcf02976ebe18dd04b163ccb571
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-10-10 03:33:53 -0300

    Aula 5 - sobre npm e módulo yargs

--- node-exemples/main.js
+++ node-exemples/main.js
@@ -1,12 +1,15 @@
 /* Aula 1 - sobre recursividade
  * Aula 2 - sobre objeto global e process
+ * Aula 5 - sobre npm e módulo yargs
  */
 
 const fatorial = require("./fatorial")
+const argv = require("yargs").demandOption("num").argv
 
 console.log("n-fatorial")
-console.log(`Caminho: ${process.cwd()}`)    // process.cwd() mostra o caminho do diretório atual
+// console.log(`Caminho: ${process.cwd()}`)  // process.cwd() mostra o caminho do diretório atual
+// const num = parseInt(process.argv[2])     // process.argv[i] array de argumentos enviados via terminal
 
-const num = parseInt(process.argv[2])   // process.argv[i] array de argumentos enviados via terminal
+const num = argv.num
 
 console.log(`Fatorial de ${num} = ${fatorial(num)}`)
--- node-exemples/save-file.js
+++ node-exemples/save-file.js
@@ -1,9 +1,18 @@
-// Aula 3 - sobre módulos node
+/* Aula 3 - sobre módulos node
+ * Aula 5 - sobre npm e módulo yargs
+ */
 
 const fs = require("fs")
+const argv = require("yargs")
+            .alias("f", "filename")     // cria o alias "f" para o parâmetro "filename"
+            .alias("c", "content")      // cria o alias "c" para o parâmetro "content"
+            .demandOption("filename")   // cria o parâmetro "filename" na constante "argv"
+            .demandOption("content")    // cria o parâmetro "content" na constante "argv"
+            .argv
 
 // função writeFile() do módulo File System cria um arquivo de texto
-fs.writeFile(process.argv[2], process.argv[3], (err) => {
+// os parâmetros argv.filename e argv.content substituem process.argv[i] de uma maneira mais concisa
+fs.writeFile(argv.filename, argv.content, (err) => {
     if(err) throw err
-    console.log(`Arquivo ${process.argv[2]} salvo com sucesso!`)
+    console.log(`Arquivo ${argv.filename} salvo com sucesso!`)
 })
