bb992043f55c51f8c04d913a19be18e469ce3578
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-10-15 05:00:33 -0300

    Aula 4 - criando classe Server e code refactoring

--- meat-app/main.ts
+++ meat-app/main.ts
@@ -1,34 +1,11 @@
-import * as restify from "restify"
+import {Server} from "./server/server"
 
-// creating server
-const server = restify.createServer({
-    name: "meat-api",
-    version: "1.0.0"
-})
+const server = new Server()
 
-// creating middleware
-server.use(restify.plugins.queryParser())
-
-// creating route
-server.get("/hello", (req, res, next) => {
-    // res.contentType = 'application/json'                 // configurando header contentType
-    // res.setHeader("Content-Type", "application/json")    // outra maneira de setar header
-    res.send({msg: "hello"})                                // método send() configura header contentType automaticamente
-    return next()
-})
-
-server.get("/info", (req, res, next) => {
-    res.json({
-        browser: req.userAgent(),
-        method: req.method,
-        url: req.url,
-        path: req.path(),
-        query: req.query
+server.bootstrap()
+    .then(server => console.log("API is running in", server.application.address()))
+    .catch(error => {
+        console.log("Server failed to start")
+        console.log(error)
+        process.exit(1)
     })
-    return next()
-})
-
-// registering listener
-server.listen(3000, () => {
-    console.log("API is running in http://localhost:3000")
-})
---
+++ meat-app/server/server.ts
@@ -0,0 +1,53 @@
+import * as restify from "restify"
+
+export class Server {
+
+    application: restify.Server
+
+    initRoutes(): Promise<any>{
+        return new Promise((resolve, reject) => {
+            try {
+
+                // creating server
+                this.application = restify.createServer({
+                    name: "meat-api",
+                    version: "1.0.0"
+                })
+
+                // creating middleware
+                this.application.use(restify.plugins.queryParser())
+
+                // creating route
+                this.application.get("/hello", (req, res, next) => {
+                    // res.contentType = 'application/json'                 // configurando header contentType
+                    // res.setHeader("Content-Type", "application/json")    // outra maneira de setar header
+                    res.send({ msg: "hello" })                                // método send() configura header contentType automaticamente
+                    return next()
+                })
+
+                this.application.get("/info", (req, res, next) => {
+                    res.json({
+                        browser: req.userAgent(),
+                        method: req.method,
+                        url: req.url,
+                        path: req.path(),
+                        query: req.query
+                    })
+                    return next()
+                })
+
+                // registering listener
+                this.application.listen(3000, () => {
+                    resolve(this.application)
+                })
+
+            } catch(error) {
+                reject(error)
+            }
+        })
+    }
+
+    bootstrap(): Promise<Server>{
+        return this.initRoutes().then(() => this)
+    }
+}
