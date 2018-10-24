6c3774508faab00e7980340b380341fa467463a0
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-10-24 12:28:02 -0300

    Implementando mongoose para conexão com MongoDB

--- meat-app/users/users.model.ts
+++ meat-app/users/users.model.ts
@@ -1,21 +1,16 @@
-const users = [
-    { id: "1", name: "Andie", email: "chuchu@andie.com" },
-    { id: "2", name: "Samuélico", email: "chuchu@samuelico.com" }
-]
+import * as mongoose from "mongoose";
 
-export class User {
-    static findAll(): Promise<any[]>{
-        return Promise.resolve(users)
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
     }
+})
 
-    static findById(id: string): Promise<any>{
-        return new Promise(resolve => {
-            const filtered = users.filter(user => user.id === id)
-            let user = undefined
-            if(filtered.length > 0){
-                user = filtered[0]
-            }
-            resolve(user)
-        })
-    }
-}
+export const User = mongoose.model("User", userSchema)
